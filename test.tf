0a9df0b52aa0cd63a9a266343f7a9f921518628b
OmerCalfon <66213075+OmerCalfon@users.noreply.github.com>
2022-05-17 17:45:30 +0300

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "amara-publicus" {
+  arn            = "arn:aws:s3:::amara-publicus"
+  bucket         = "omer-publicus"
+  force_destroy  = "false"
+  hosted_zone_id = var.hosted_zone_id
+  request_payer  = "BucketOwners"
+
+  versioning {
+    enabled    = "true"
+    mfa_delete = var.mfa_enabled
+  }
+}
